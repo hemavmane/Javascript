c3ade8aa628122152cfaad55b6f370d004ca588d
hemavmane <hemamane5376@gmail.com>
2023-02-24 23:08:12 +0530

    added

---
+++ Arrow-Function/arrow.js
@@ -0,0 +1,12 @@
+// let sum =(greet)=>{
+//     console.log(greet)
+// }
+
+// sum("hello")
+
+
+//////////////////////
+// without parenthesis and return 
+let data = greet => greet
+
+console.log(data("hi"))
